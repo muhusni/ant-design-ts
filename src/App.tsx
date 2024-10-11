ac0f0b415ddc1b2b306a023814c37e27ec613779
Muhamad Husni <unny.husni@gmail.com>
2024-10-11 22:55:44 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Layout from "./Layout/layout"
+import "./index.css"
+function App() {
+
+  return (
+    <>
+      <Layout/ >
+    </>
+  )
+}
+
+export default App
---
+++ src/Layout/layout.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import {
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+    UploadOutlined,
+    UserOutlined,
+    VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Button, Layout, Menu, theme } from 'antd';
+
+const { Header, Sider, Content } = Layout;
+
+const App: React.FC = () => {
+    const [collapsed, setCollapsed] = useState(false);
+    const {
+        token: { colorBgContainer, borderRadiusLG },
+    } = theme.useToken();
+
+    return (
+        <Layout style={{ minHeight: "100vh" }}>
+            <Sider trigger={null} collapsible collapsed={collapsed}>
+                <div className="demo-logo-vertical">APP</div>
+                <Menu
+                    theme="dark"
+                    mode="inline"
+                    defaultSelectedKeys={['1']}
+                    items={[
+                        {
+                            key: '1',
+                            icon: <UserOutlined />,
+                            label: 'nav 1',
+                        },
+                        {
+                            key: '2',
+                            icon: <VideoCameraOutlined />,
+                            label: 'nav 2',
+                        },
+                        {
+                            key: '3',
+                            icon: <UploadOutlined />,
+                            label: 'nav 3',
+                        },
+                    ]}
+                />
+            </Sider>
+            <Layout>
+                <Header style={{
+                    display: 'flex',
+                    justifyContent: 'space-between',  alignItems: 'center', padding: 0, background: colorBgContainer
+                }}>
+                    <Button
+                        type="text"
+                        icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                        onClick={() => setCollapsed(!collapsed)}
+                        style={{
+                            fontSize: '16px',
+                            width: 64,
+                            height: 64,
+                        }}
+                    />
+
+                    <Button style={{
+                        fontSize: '16px',
+                        marginRight: "10px"
+                    }}>Another Button</Button>
+                </Header>
+                <Content
+                    style={{
+                        margin: '24px 16px',
+                        padding: 24,
+                        minHeight: 280,
+                        background: colorBgContainer,
+                        borderRadius: borderRadiusLG,
+                    }}
+                >
+                    Content
+                </Content>
+            </Layout>
+        </Layout>
+    );
+};
+
+export default App;
